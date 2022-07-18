35a3e6fa0b171023bd60196fd0586154e6fad745
Nazim Bayraktar <nab1079@hotmail.com>
2022-07-18 23:29:06 +0100

    add removed some html tags in index.html, style.css

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
-const alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const numbers = "0123456789";
-const symbols = "!@#$%^&*_-+=";
+const btnGenerate = document.getElementById('btn-generate');
+const upperEl = document.getElementById('uppercase');
+const lowerEl = document.getElementById('lowercase');
+const symbolsEl = document.getElementById('symbols');
+const numbersEl = document.getElementById('numbers');
+const alphaUpper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const alphaLower = 'abcdefghijklmnopqrstuvwxyz';
+const syms = '!@#$%^&*_-+=';
+const nums = '0123456789';
+const passLength = 10;
+
+const passBox1 = document.getElementById('pass-1');
+const passBox2 = document.getElementById('pass-2');
+const passBox3 = document.getElementById('pass-3');
+const passBox4 = document.getElementById('pass-4');
+
+btnGenerate.addEventListener('click', () => {
+  let characters = '';
+  upperEl.checked ? (characters += alphaUpper) : '';
+  lowerEl.checked ? (characters += alphaLower) : '';
+  symbolsEl.checked ? (characters += syms) : '';
+  numbersEl.checked ? (characters += nums) : '';
+
+  for (let i = 0; i < passLength; i++) {
+    let password = '';
+
+    password += characters.charAt(Math.floor(Math.random() * characters.length));
+
+  }
+
+
+})
+
+function showPass(arg) {
+
+}
