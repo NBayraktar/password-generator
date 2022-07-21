bd351c7fe5d0044bda86fbb54502e7006baa65b1
Nazim Bayraktar <nab1079@hotmail.com>
2022-07-21 20:38:54 +0100

    changed password min length to 3, and add logic with javascript in script.js

--- script.js
+++ script.js
@@ -20,10 +20,72 @@ const randomFunc = {
   number: getRandomNumber
 }
 
+// Reset result 
+resetEl.addEventListener('click', () => {
+  resultEl.textContent = '• • • •';
+})
+
+// Generate event listener
 btnGenerate.addEventListener('click', () => {
   const length = Number(lengthEl.value);
-  console.log(typeof length);
+  const hasUpper = upperEl.checked;
+  const hasLower = lowerEl.checked;
+  const hasSymbol = symbolsEl.checked;
+  const hasNumber = numbersEl.checked;
+  resultEl.innerText = generatePassword(length, hasUpper, hasLower, hasSymbol, hasNumber);
 })
+
+// Copy password to clipboard
+clipboardEl.addEventListener('click', () => {
+  const textarea = document.createElement('textarea');
+  const password = resultEl.innerText;
+
+  if (!password) {
+    return '';
+  }
+  textarea.value = password;
+  document.body.appendChild(textarea);
+  textarea.select();
+  document.execCommand('copy');
+  textarea.remove();
+  alert('Password copied to clipboard!');
+})
+
+// Generate password function
+function generatePassword(length, upper, lower, symbol, number) {
+  // 1. Init pass variable;
+  // 2. Filter out unchecked types;
+  // 3. Loop over length call generator function for each type;
+  // 4. Add final pass to the pass variable and return;
+  let generatedPassword = '';
+  const typesCount = upper + lower + symbol + number;
+  const typesArr = [{
+    upper
+  }, {
+    lower
+  }, {
+    symbol
+  }, {
+    number
+  }].filter(item => Object.values(item)[0]);
+  // console.log('typesCount: ', typesCount);
+  // console.log('typesArr: ', typesArr);
+
+  if (typesCount === 0) {
+    return '';
+  }
+
+  for (let i = 0; i < length; i += typesCount) {
+    typesArr.forEach(type => {
+      const funcName = Object.keys(type)[0];
+      // console.log(funcName);
+      generatedPassword += randomFunc[funcName]();
+    });
+  }
+  const finalPassword = generatedPassword.slice(0, length);
+  return finalPassword;
+}
+
 
 // Generator functions
 function getRandomUpper() {
